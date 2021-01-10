ff2785ef44c60ecd26d3261d5a94bc14d0d1d1c6
NeilRelatado <neilrelatado@live.com>
2021-01-10 15:53:57 -0500

    background image change edit

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-var images=new Array('images/img1.png','images/img2.png','images/img3.png');
+var images=new Array('images/img1.png','images/img2.png','images/img3.png','images/church.png');
 var nextimage=0;
 
-doSlideshow();
+changeBack();
 
-function doSlideshow()
-{
-    if($('.slideshowimage').length!=0)
-    {
-        $('.slideshowimage').fadeOut(2000,function(){slideshowFadeIn();$(this).remove()});
-    }
-    else
-    {
-        slideshowFadeIn();
-    }
+
+
+
+
+
+function next(){
+  nextimage +=1
+  if (nextimage >= images.length){
+    nextimage=0;
+  }
+  console.log(nextimage)
+  console.log("Hello")
 }
-function slideshowFadeIn()
+function changeBack()
 {
-    $('.top-header').prepend($('<img class="slideshowimage" src="'+images[nextimage++]+'" style="display:none">').fadeIn(1000,function(){setTimeout(doSlideshow,1000);}));
-    if(nextimage>=images.length)
-        nextimage=0;
+    $('.top-header').css("background-image", "url("+images[nextimage]+")")
+    setInterval(function(){
+  next();
+
+  $('.top-header').css("background-image", "url("+images[nextimage]+")")
+  }, 5000);
 }
