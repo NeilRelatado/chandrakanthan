4130ace27ced74858ed24def02d1c51a1656a85d
NeilRelatado <neilrelatado@live.com>
2021-06-09 22:32:51 -0400

    Big Updates
    -Changed Bg
    -Removed JS for now
    -Changed Font sizes and centered

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
-var images=new Array('images/img1.png','images/img2.png','images/img3.png','images/church.png');
-var nextimage=0;
-
-changeBack();
 
 
-
-
-
-
-function next(){
-  nextimage +=1
-  if (nextimage >= images.length){
-    nextimage=0;
-  }
-  console.log(nextimage)
-  console.log("Hello")
-}
-function changeBack()
-{
-    $('.top-header').css("background-image", "url("+images[nextimage]+")")
-    setInterval(function(){
-  next();
-
-  $('.top-header').css("background-image", "url("+images[nextimage]+")")
-  }, 5000);
-}
